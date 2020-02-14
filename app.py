e2e3b91319b4e2bdb64c44f77dd86b458ddee4e2
Alexander Litovchenko <yazmolod@gmail.com>
2020-02-14 17:30:13 +0400

    some improves

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
+import openpyxl
+
 from configBuilder import ConfigConstructor
 
 class workTableModel(QAbstractTableModel):
-    def __init__(self, parent=None):        
+    def __init__(self, parent=None):      
         super().__init__(parent)
-        self.innerData = [["","","",""]]
+        self.columns = ("N", "Name", "Unit", "Value")
+        self.innerData = []
 
     def addData(self, data):        
         self.innerData += data
@@ -22,13 +25,14 @@ class workTableModel(QAbstractTableModel):
         return len(self.innerData)
 
     def columnCount(self, *args):
-        return len(self.innerData[0])
+        return len(self.columns)
 
     def data(self, index, role=Qt.DisplayRole):
         if role == Qt.DisplayRole:
             i = index.row()
             j = index.column()
-            return '{0}'.format(self.innerData[i][j])
+            column_name = self.columns[j]
+            return self.innerData[i].get(column_name, "")
         else:
             return QVariant()
 
@@ -100,14 +104,15 @@ class CentralWidget(QWidget):
         dialog = InputDataDialog(inputDataConfig)
         ret = dialog.exec_()
         if ret == QDialog.Accepted:
-            self.addNewWork(dialog.tableResult)
+            self.addNewWork(dialog.outputs)
 
     def addNewWork(self, tableResult):
         self.tableModel.addData(tableResult)
         self.tableView.update()
 
-    def export_clicked(self):
-        print ('Export excel')
+    def export_clicked(self):        
+        data = self.tableModel.innerData
+
 
 
 
@@ -118,6 +123,7 @@ class InputDataDialog(QDialog):
         loadUi(r"./ui/inputDialog.ui", self)
         self.inputs = []
         self.outputs = []
+        self.sectionData = None
         self.backButton.setEnabled(False)
         self.config = config
         self.loadSectionSheet(self.Stack.widget(0), [i.get("Name") for i in config])
@@ -130,7 +136,7 @@ class InputDataDialog(QDialog):
         self.acceptButton.clicked.connect(self.acceptButton_clicked)
         self.buttonGroup.buttonClicked.connect(self.radioButtonGroup_clicked)
 
-    def loadSectionSheet(self, parent, data):
+    def loadSectionSheet(self, parent, data):        
         layout = QVBoxLayout(parent)
         self.buttonGroup = QButtonGroup(parent)
         for section in data:
@@ -138,7 +144,8 @@ class InputDataDialog(QDialog):
             self.buttonGroup.addButton(radio)
             layout.addWidget(radio)
 
-    def loadInputSheet(self, parent, data):
+    def loadInputSheet(self, parent):
+        data = self.sectionData["Inputs"]
         self.inputs.clear()
         layout = QGridLayout(parent)
         for i in range(len(data)):
@@ -151,15 +158,18 @@ class InputDataDialog(QDialog):
             layout.addWidget(le, i, 1)
             layout.addWidget(measureLabel, i, 2)
 
-    def loadOutputSheet(self, parent, data):
+    def loadOutputSheet(self, parent):
+        data = self.sectionData["Outputs"]
         layout = QGridLayout(parent)
         self.outputs.clear()
         for i in range(len(data)):
-            nameLabel = QLabel(data[i].get("Name"), parent)
-            measureLabel = QLabel(data[i].get("Unit"), parent)
+            name = data[i].get("Name")
+            unit = data[i].get("Unit")
+            nameLabel = QLabel(name, parent)
+            measureLabel = QLabel(unit, parent)
             
             le = QDoubleSpinBox(parent)
-            self.outputs.append(le)
+            self.outputs.append({"Name": name, "Unit": unit, "UI": le})
             le.formula = data[i].get("Formula", "").replace("[", "self.inputs[").replace("]", "].value()")
             le.setValue(0)
 
@@ -170,9 +180,8 @@ class InputDataDialog(QDialog):
 
     def updateOutputs(self, *args):
         for o in self.outputs:
-            print (o.formula)
-            exec('global xxx; xxx = ' + o.formula)
-            o.setValue(xxx)
+            exec('global xxx; xxx = ' + o['UI'].formula)
+            o['UI'].setValue(xxx)
 
 
     def radioButtonGroup_clicked(self, btn):
@@ -182,8 +191,9 @@ class InputDataDialog(QDialog):
         if inputLayout != None:
             for i in reversed(range(inputLayout.count())): 
                 inputLayout.itemAt(i).widget().setParent(None)
-        self.loadInputSheet(inputWidget, [i for i in self.config if i['Name'] == btn.text()][0]['Inputs'])
-        self.loadOutputSheet(outputWidget, [i for i in self.config if i['Name'] == btn.text()][0]['Outputs'])
+        self.sectionData = [i for i in self.config if i['Name'] == btn.text()][0]
+        self.loadInputSheet(inputWidget)
+        self.loadOutputSheet(outputWidget)
 
 
     def nextButton_clicked(self):
@@ -199,7 +209,8 @@ class InputDataDialog(QDialog):
         self.backButton.setEnabled(curInd - 1 != 0 )
 
     def acceptButton_clicked(self):
-        self.tableResult = [["", "", "", i.value()] for i in self.outputs]
+        for i in self.outputs:
+            i['Value'] = i['UI'].value()        
         self.accept()
 
 
--- configBuilder.py
+++ configBuilder.py
@@ -3,8 +3,39 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
-class TreeModel(QAbstractProxyModel):
-	pass
+class WorkItem():
+	def __init__(self):
+		pass
+
+class SectionItem():
+	def __init__(self):
+		pass
+
+class InputItem():
+	def __init__(self, name, unit):
+		self._name = name
+		self._unit = unit
+
+class OutputItem():
+	def __init__(self, name, unit, formula):
+		self._name = name
+		self._unit = unit
+		self._formula = formula
+
+class TreeNode(object):
+	def __init__(self, parent, row):
+		self._parent = parent
+		self._row = row
+		self._children = self._getChildren()
+
+	def _getChildren(self):
+		pass
+
+
+class TreeModel(QAbstractItemModel):
+	def __init__(self):
+		super().__init__(self)
+
 
 
 class ConfigConstructor(QDialog):
